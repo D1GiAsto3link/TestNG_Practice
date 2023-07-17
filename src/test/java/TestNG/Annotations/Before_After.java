e4c7e84d73090bd3c0c236cff66a7b9a5acc214a
D1GiAsto3link <Samad@digilink.africa>
2023-07-17 16:18:02 +0200

    coomet

--- src/test/java/TestNG/Annotations/Before_After.java
+++ src/test/java/TestNG/Annotations/Before_After.java
@@ -2,10 +2,16 @@ package TestNG.Annotations;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 public class Before_After {
+    WebDriver driver;
+
         @Test
         public void verifySetup()
         {
@@ -19,5 +25,22 @@ public class Before_After {
 //Write your test case logic here.
     }
 
+@BeforeTest
+    public void browserSetup()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
 
+        //Code to maximize the browser
+    }
+
+
+
+    @AfterTest
+    public void compileReport()
+    {
+        driver.quit();
+//code to compile a report
+    }
 }
